087dd59b600c858fc8fa34ebd9650fafb41b599d
fgumus20 <fgumus20@ku.edu.tr>
2026-03-05 22:46:11 +0300

    Added billboard logic for rotation

---
+++ Assets/_Scripts/UI/Billboard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform camTransform;
+
+    void Start()
+    {
+        camTransform = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = camTransform.rotation;
+    }
+}
